c49f81413ac279a51f9bf29c07df5a34ce738382
MGDB-DiabolicScript <sasukemoises55@gmail.com>
2019-11-14 13:08:06 +0100

    des

---
+++ Assets/materiales/gonzalezmoises@cuaderno.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gonzalezmoises@cuaderno : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+// propiedades de la esfera
+//El componente Transformar determina la posición, la rotación y la escala de cada objeto en la escena.
+//Rotacion:permite mover por los 3 ejes el objeto.
+//Escala:El valor "1" es el tamaño original, Nos permite cambiar el tamaño del objeto.
+//Posicion:permite mover el objeto puesto en escena atra vez de los ejes X,Y,Z.
