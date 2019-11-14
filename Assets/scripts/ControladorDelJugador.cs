b816a678335574c92d50c2c210b4c5b9a3d60815
MGDB-DiabolicScript <sasukemoises55@gmail.com>
2019-11-14 14:25:20 +0100

    des

---
+++ Assets/scripts/ControladorDelJugador.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ControladorDelJugador : MonoBehaviour
+{
+    public float velocidad;
+    Rigidbody rb;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        float movimientoHorizontal = Input.GetAxis("Horizontal");
+        float movimientoVertical = Input.GetAxis("Vertical");
+        Vector3 movimiento = new Vector3(movimientoHorizontal, 0f,
+        movimientoVertical);
+        rb.AddForce(movimiento * velocidad);
+    }
+}
--- Assets/scripts/gonzalezmoises@cuaderno.cs
+++ Assets/scripts/gonzalezmoises@cuaderno.cs
@@ -2,22 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class gonzalezmoises@cuaderno : MonoBehaviour
-{
+//public class gonzalezmoises@cuaderno : MonoBehaviour{
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //void Start(){}
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+    //void Update(){}}
+
 // propiedades de la esfera
 //El componente Transformar determina la posición, la rotación y la escala de cada objeto en la escena.
 //Rotacion:permite mover por los 3 ejes el objeto.
 //Escala:El valor "1" es el tamaño original, Nos permite cambiar el tamaño del objeto.
 //Posicion:permite mover el objeto puesto en escena atra vez de los ejes X,Y,Z.
+//addforce:permite dar mayor fuerza de rigidbody haciendo que coja velocidad o se relentice
