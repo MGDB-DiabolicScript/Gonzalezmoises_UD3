ac0cc5c24369ff863853194f794f59a601c670b2
MGDB-DiabolicScript <sasukemoises55@gmail.com>
2019-12-05 09:51:40 +0100

    5/12/2019

---
+++ Assets/scripts/MovementScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public GameObject destino;
+    private UnityEngine.AI.NavMeshAgent agenteDeNavegacion;
+    // Use this for initialization
+    void Start()
+    {
+        agenteDeNavegacion = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agenteDeNavegacion.SetDestination(destino.transform.position);
+    }
+}
